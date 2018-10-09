2cb2dc3c1821f62151ad2975b51a1bace03e2598
akbar <akbar.nimatov@mail.ru>
2018-10-09 10:55:16 +0500

    #Added : logs of security filter chain, log4j2

--- producer/src/main/java/uz/oltinolma/producer/files/controller/FileController.java
+++ producer/src/main/java/uz/oltinolma/producer/files/controller/FileController.java
@@ -1,6 +1,7 @@
 package uz.oltinolma.producer.files.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uz.oltinolma.producer.files.dao.FileDao;
@@ -13,12 +14,14 @@ public class FileController {
     @Autowired
     FileDao fileDao;
 
+    @PreAuthorize("@SecurityPermission.hasPermission('file.upload')")
     @PostMapping(value = "/uploadFile")
     public Object uploadFile(@RequestParam("file") MultipartFile file
     ) {
         return fileDao.saveFile(file, UUID.randomUUID());
     }
 
+    @PreAuthorize("@SecurityPermission.hasPermission('file.info')")
     @GetMapping("/getFile/{id}")
     public Object info(@PathVariable UUID id) {
         return fileDao.getFileFromId(id);
