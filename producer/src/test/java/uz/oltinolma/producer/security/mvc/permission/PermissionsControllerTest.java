f9327ccef645a94f38afd8e30c11354a3eb2464f
valijon <mr.valijonn@gmail.com>
2018-10-25 17:53:25 +0500

    Update PermissionsControllerTest
    When unauthorized case is completed.

--- producer/src/test/java/uz/oltinolma/producer/security/mvc/permission/PermissionsControllerTest.java
+++ producer/src/test/java/uz/oltinolma/producer/security/mvc/permission/PermissionsControllerTest.java
@@ -1,7 +1,10 @@
 package uz.oltinolma.producer.security.mvc.permission;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +23,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static uz.oltinolma.producer.security.UserDummies.authorizedUser;
 import static uz.oltinolma.producer.security.UserDummies.guestUser;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = SecurityTestConfig.class)
@@ -61,6 +64,12 @@ class PermissionsControllerTest {
             tokenForAdmin = "Bearer " + tokenHelper.normalTokenForAdmin();
             given(permissionService.getByLogin(authorizedUser().getLogin())).willReturn(permissionNames);
             given(permissionService.list()).willReturn(permissionDummies.getAll());
+            for (Permission p : permissionDummies.getAll()) {
+                given(permissionService.get(p.getId())).willReturn(p);
+            }
+
+
+
         }
 
 
@@ -69,7 +78,7 @@ class PermissionsControllerTest {
         void getList_MustReturnAllPermissions() throws Exception {
             mockMvc.perform(get("/permissions/list").headers(headers()))
                     .andDo(print())
-                    .andExpect(status().is(200))
+                    .andExpect(status().isOk())
                     .andExpect(content().json(mapper.writeValueAsString(permissionDummies.getAll())));
         }
 
@@ -78,9 +87,21 @@ class PermissionsControllerTest {
         void getListByLogin_MustReturnPermissionNamesForCurrentUser() throws Exception {
             mockMvc.perform(get("/permissions/list/for/current/user").headers(headers()))
                     .andDo(print())
-                    .andExpect(status().is(200))
+                    .andExpect(status().isOk())
                     .andExpect(content().json(mapper.writeValueAsString(permissionNames)));
 
+        }
+
+        @Test
+        @DisplayName("getById returns permission object for given id")
+        void getById() throws Exception {
+            for (Permission p : permissionDummies.getAll())
+                mockMvc.perform(get("/permissions/{id}", p.getId()).headers(headers()))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("id").value(p.getId()))
+                        .andExpect(jsonPath("name").value(p.getName()))
+                        .andExpect(jsonPath("info").value(p.getInfo()));
         }
 
         private HttpHeaders headers() {
@@ -97,6 +118,7 @@ class PermissionsControllerTest {
         private String tokenForGuest;
 
         WhenUnauthorized() {
+            permissionDummies = new PermissionDummies();
             given(userService.findByLogin(guestUser().getLogin())).willReturn(guestUser());
             tokenHelper.setUserService(userService);
             tokenForGuest = "Bearer " + tokenHelper.normalTokenForGuest();
@@ -107,7 +129,6 @@ class PermissionsControllerTest {
         @DisplayName("getList 403")
         void getList() throws Exception {
             mockMvc.perform(get("/permissions/list").headers(headers()))
-                    .andDo(print())
                     .andExpect(status().isForbidden());
         }
 
@@ -115,7 +136,40 @@ class PermissionsControllerTest {
         @DisplayName("getListByLogin 403")
         void getListByLogin_MustReturnPermissionNamesForCurrentUser() throws Exception {
             mockMvc.perform(get("/permissions/list/for/current/user").headers(headers()))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        @DisplayName("getById 403")
+        void getById() throws Exception {
+            for (Permission p : permissionDummies.getAll())
+                mockMvc.perform(get("/permissions/{id}", p.getId()).headers(headers()))
+                        .andExpect(status().isForbidden());
+        }
+
+        @Test
+        @DisplayName("update 403")
+        void update() throws Exception {
+            String jsonPermissionToUpdate = mapper.writeValueAsString(permissionDummies.getAll().get(0));
+            mockMvc.perform(put("/permissions")
+                        .headers(headers())
+                        .content(jsonPermissionToUpdate))
                     .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        @DisplayName("insert 403")
+        void insert() throws Exception {
+            String jsonPermissionToInsert = mapper.writeValueAsString(new Permission());
+            mockMvc.perform(post("/permissions")
+                        .headers(headers())
+                        .content(jsonPermissionToInsert))
+                    .andExpect(status().isForbidden());
+        }
+
+        void deleteTest() throws Exception {
+            mockMvc.perform(delete("/permissions").headers(headers()))
                     .andExpect(status().isForbidden());
         }
 
