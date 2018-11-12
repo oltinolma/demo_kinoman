3febac0b9bd1fec9f7bf69aa79ea7978b72d3dba
akbar <akbar.nimatov@mail.ru>
2018-11-12 11:09:53 +0500

    Delete unused classes.
    Fix database logic to collect movies.
    Do refactoring wrapper classes.

--- consumer/src/main/java/uz/oltinolma/consumer/mvc/movie/service/MovieServiceImpl.java
+++ consumer/src/main/java/uz/oltinolma/consumer/mvc/movie/service/MovieServiceImpl.java
@@ -1,17 +1,17 @@
 package uz.oltinolma.consumer.mvc.movie.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import uz.oltinolma.consumer.mvc.config.SearchResult;
+import uz.oltinolma.consumer.mvc.movie.MovieWrapper;
 import uz.oltinolma.consumer.mvc.movie.dao.MovieDao;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -31,33 +31,27 @@ public class MovieServiceImpl implements MovieService {
     }
 
     @Override
-    public String movieInfoById(UUID id) {
-        return String.valueOf(movieDao.info(id)).replace("''", "'");
-
-    }
-
-    @Override
     public void insert(String json) {
         movieDao.insert(json);
     }
 
     @Override
-    public List<Object> list() {
+    public List<MovieWrapper> list() {
         return movieDao.list();
     }
 
     @Override
-    public Object getMoviesListFromRequestedTaxonomiesForMenu(List<String> taxonomies) {
+    public List<MovieWrapper> getMoviesListFromRequestedTaxonomiesForMenu(List<String> taxonomies) {
         return movieDao.getMoviesListFromRequestedTaxonomiesForMenu(taxonomies);
     }
 
     @Override
-    public HashMap<String, Object> getMovieAsObject(UUID id) {
-        return (HashMap<String, Object>) movieDao.getMovieAsObject(id);
+    public MovieWrapper getMovieAsObject(UUID id) {
+        return movieDao.getMovieAsObject(id);
     }
 
     @Override
-    public Object getMovieListFromRequestedTaxonomies(String taxonomy) {
+    public List<MovieWrapper> getMovieListFromRequestedTaxonomies(String taxonomy) {
         String resourceUrl
                 = "http://192.168.0.50:7576/search/for/movie/or/taxonomy";
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(resourceUrl)
@@ -70,6 +64,8 @@ public class MovieServiceImpl implements MovieService {
                 });
         List<String> taxonomies = response.getBody().stream().filter(t -> "taxonomy".equals(t.getType())).map(SearchResult::getTerm).collect(Collectors.toList());
         List<String> movies = response.getBody().stream().filter(t -> "movie".equals(t.getType())).map(SearchResult::getTerm).collect(Collectors.toList());
-        return movieDao.getMovieListFromRequestedTaxonomies(taxonomies, movies);
+        List<MovieWrapper> movieWrappers = movieDao.getMovieListFromRequestedTaxonomies(taxonomies, movies);
+        movieWrappers.removeAll(Collections.singleton(null));
+        return movieWrappers;
     }
 }
